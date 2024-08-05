fe3ec861b413da220ab68215580a1f39f42a6241
Ana Benítez <abenitez81@gmail.com>
2024-08-06 00:27:18 +0200

    update scripts

--- scripts/02_FD_steppe.R
+++ scripts/02_FD_steppe.R
@@ -1,1 +1,24 @@
-kskas
+# Functional Diversity analysis for steppe birds
+# Date: 05/08/2024
+# Author: Ana Benítez
+
+
+library(dplyr)
+library(tidyr)
+library(tibble)
+library(mFD)
+library(ggplot2)
+
+#Import data
+
+sp_data <- read.csv("data/TraitsSteppebirdsFull.csv")
+
+biomass_matrix <- fauna %>% group_by(SITE, REFERENCE_TAXA) %>% 
+  summarize(BIOMASS=sum(BIOMASS_KG)) %>%
+  spread(key = REFERENCE_TAXA,value = BIOMASS)%>% #biomass reported in kg
+  replace(is.na(.), 0) %>% #replace NAs with 0
+  tibble::column_to_rownames(var = "SITE")
+
+rm(fauna) #clean environment
+
+#Create matrix depicting vertebrate's traits
